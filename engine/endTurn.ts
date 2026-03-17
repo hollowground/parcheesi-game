dedcb4240d061a9e866443cb8626be23358f3423
Matt K <mattgkay@gmail.com>
2026-03-17 06:26:11 -0400

    Updated to support the first doubles rules test to pass

--- engine/endTurn.ts
+++ engine/endTurn.ts
@@ -1,5 +1,24 @@
 import { GameState } from "../types/GameState"
 
 export function endTurn(state: GameState): GameState {
-    return state
+
+    const newState = structuredClone(state)
+
+    const [d1, d2] = newState.dice
+
+    const isDouble = d1 === d2
+
+    if (isDouble) {
+        newState.consecutiveDoubles += 1
+        return newState
+    }
+
+    newState.consecutiveDoubles = 0
+
+    const currentIndex = newState.players.indexOf(newState.currentPlayer)
+    const nextIndex = (currentIndex + 1) % newState.players.length
+
+    newState.currentPlayer = newState.players[nextIndex]!
+
+    return newState
 }
